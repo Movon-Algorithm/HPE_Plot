78162466d76cb70641800222ac6c1a256a3b406e
Changmin Yi <ycm93dg@gmail.com>
2024-07-12 13:34:51 +0900

    [feat] add file list read

---
+++ example/read_file_list.py
@@ -0,0 +1,11 @@
+﻿import sys, os
+import pdb
+
+file_path = "Y:\\20240712_MDSM_DEMO\\20240711_180801_MVT901\\results"
+file_list = os.listdir(file_path)
+file_list = sorted(file_list)
+
+for file in file_list:
+    with open(os.path.join(file_path, file), "r") as f:
+        pdb.set_trace()
+        print(f.read())
